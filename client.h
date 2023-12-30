ce87801714e9a5e353d3d0c2c774bae074b2056b
XUXINGPAN <3344255949@qq.com>
2023-12-31 01:13:13 +0800

    first commit

---
+++ client.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <bits/pthreadtypes.h>
+
+#define BUFFSIZE 128
+#define HOST_IP "192.168.56.101"
+#define PORT 8888
+
+
+int sockfd;
+void snd();
+void get_help();
